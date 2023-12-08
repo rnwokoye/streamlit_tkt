d40e4d50bd4a89b24a4606ac553c798b3f6eb4ac
Richard Nwokoye <RNwokoye@gmail.com>
2023-12-07 19:04:56 -0500

    first commit

--- app.py
+++ app.py
@@ -11,23 +11,24 @@ if "session_data" not in st.session_state:
 
 def my_function():
     st.write(f"Current session state data is: {st.session_state.session_data}")
+    st.write("Hello World")
     return
 
 
-with st.form(key="form"):
-    user_name = st.text_input("Text")
-    if user_name:
-        st.session_state.session_data["user_name"] = user_name
-        submit = st.form_submit_button()
+# with st.form(key="form"):
+#     user_name = st.text_input("Text")
+#     if user_name:
+#         st.session_state.session_data["user_name"] = user_name
+#     submit = st.form_submit_button()
 
-    if submit:
-        st.write(f"Text: {user_name}")
+#     if submit:
+#         st.write(f"Text: {user_name}")
 
 
-st.write("Pandas Testing")
-data = np.arange(0, 100, 2).reshape(5, 10)
-df = pd.DataFrame(data)
-st.write(df)
+# st.write("Pandas Testing")
+# data = np.arange(0, 100, 2).reshape(5, 10)
+# df = pd.DataFrame(data)
+# st.write(df)
 
 
 if __name__ == "__main__":
